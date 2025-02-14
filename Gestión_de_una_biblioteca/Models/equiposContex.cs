da09c9e256556e76a726cbae57db3ebd9f186687
HenryPresidente <Henry@codersfree.com>
2025-02-13 19:40:03 -0600

    contexto

---
+++ Gestión_de_una_biblioteca/Models/equiposContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace Gestión_de_una_biblioteca.Models
+{
+    public class equiposContex: DbContext
+    {
+        public equiposContex(DbContextOptions<equiposContex>options) : base(options)
+        { 
+         
+        }
+
+    }
+}
--- Gestión_de_una_biblioteca/Program.cs
+++ Gestión_de_una_biblioteca/Program.cs
@@ -1,8 +1,14 @@
+using Gestión_de_una_biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<equiposContex>(options =>
+    options.UseSqlserver(;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
